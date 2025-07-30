15cd2b2a16854492371a44784b2e52cbc610e742
Simone Orsi <simahawk@gmail.com>
2025-07-30 21:30:10 +0200

    ebill_postfinance: migrate to v18

--- ebill_postfinance/models/ebill_postfinance_invoice_message.py
+++ ebill_postfinance/models/ebill_postfinance_invoice_message.py
@@ -281,14 +281,14 @@ class EbillPostfinanceInvoiceMessage(models.Model):
         amount_by_group = []
         # Get the percentage of the tax from the name of the group
         # Could be improve by searching in the account_tax linked to the group
-        for __, tax_group in self.invoice_id.tax_totals["groups_by_subtotal"].items():
-            for taxgroup in tax_group:
-                rate = taxgroup["tax_group_name"].split()[-1:][0][:-1]
+        for subtotal in self.invoice_id.tax_totals["subtotals"]:
+            for taxgroup in subtotal["tax_groups"]:
+                rate = taxgroup["group_name"].split()[-1:][0][:-1]
                 amount_by_group.append(
                     (
                         rate or "0",
-                        taxgroup["tax_group_amount"],
-                        taxgroup["tax_group_base_amount"],
+                        taxgroup["tax_amount"],
+                        taxgroup["base_amount"],
                     )
                 )
         params["amount_by_group"] = amount_by_group
